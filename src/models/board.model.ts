c8ccf68fa01d3a2c6da5c59e1aeee0bf1b4da77c
a-maystorov <alkin971@gmail.com>
2024-10-26 17:50:11 +0200

    feat: add board model

---
+++ src/models/board.model.ts
@@ -0,0 +1,29 @@
+import mongoose, { Document, Schema, Types } from 'mongoose';
+
+export interface IBoard extends Document {
+  name: string;
+  ownerId: Types.ObjectId;
+  columns: {
+    name: string;
+    tasks: Types.ObjectId[];
+  }[];
+}
+
+const BoardSchema: Schema = new Schema({
+  name: { type: String, required: true },
+  ownerId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true,
+  },
+  columns: [
+    {
+      name: { type: String, required: true },
+      tasks: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Task' }],
+    },
+  ],
+});
+
+const Board = mongoose.model<IBoard>('Board', BoardSchema);
+
+export default Board;
