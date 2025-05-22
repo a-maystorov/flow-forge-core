c93e275f88f0664783dc3aecbf790ba91a92e38a
a-maystorov <alkin971@gmail.com>
2025-05-22 14:20:49 +0200

    fix: tests

--- jest.config.ts
+++ jest.config.ts
@@ -6,6 +6,11 @@ const config: Config = {
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
   testMatch: ['**/__tests__/**/*.test.ts'],
   verbose: true,
+  globals: {
+    'ts-jest': {
+      tsconfig: 'tsconfig.test.json',
+    },
+  },
 };
 
 export default config;
