a70910c7c3fa51835b3b40f513e42e519e85aa66
a-maystorov <alkin971@gmail.com>
2025-06-05 14:08:31 +0200

    feat: setup socket io

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
 import dotenv from 'dotenv';
 import express from 'express';
 // Only importing for type information, not for runtime
+import { join } from 'path';
 import type {} from '../@types/express';
 import corsMiddleware from './config/cors';
 import { connectDB } from './config/database';
@@ -8,6 +9,8 @@ import { errorHandler } from './middleware';
 import authRoutes from './routes/auth.routes';
 import boardRoutes from './routes/board.routes';
 import userRoutes from './routes/user.routes';
+import { Server } from 'socket.io';
+import { createServer } from 'http';
 
 dotenv.config();
 
@@ -16,6 +19,9 @@ if (process.env.NODE_ENV !== 'test') {
 }
 
 const app = express();
+const server = createServer(app);
+const io = new Server(server);
+
 const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
@@ -24,6 +30,16 @@ app.use(corsMiddleware);
 app.use('/api/users', userRoutes);
 app.use('/api/auth', authRoutes);
 app.use('/api/boards', boardRoutes);
+
+app.get('/chat-test', (req, res) => {
+  res.sendFile(join(__dirname, 'index.html'));
+});
+
+io.on('connection', (socket) => {
+  socket.on('chat message', (msg) => {
+    io.emit('chat message', msg);
+  });
+});
 
 app.get('/', async (req, res) => {
   res.send('Hello, Flow Forge Core!');
@@ -41,7 +57,7 @@ app.get('/api', (req, res) => {
 app.use(errorHandler);
 
 if (process.env.NODE_ENV !== 'test') {
-  app.listen(PORT, () => {
+  server.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
   });
 }
